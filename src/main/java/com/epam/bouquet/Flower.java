c0705fe618e747e8f1ae166d5b6cf22c7be6a9ff
Andriy <andriymattt@gmail.com>
2019-12-28 00:12:13 +0200

    feature/decorator: add the Flower enum

---
+++ src/main/java/com/epam/bouquet/Flower.java
@@ -0,0 +1,16 @@
+package com.epam.bouquet;
+
+public enum Flower {
+    ROSE(25), TULIP(10), LILY(25), DAISY(15), SNOWDROP(10), POPPY(12),
+    BLUEBELL(8), SUNFLOWER(30), IRIS(15);
+
+    final private double PRICE;
+
+    private Flower(double price) {
+        this.PRICE = price;
+    }
+
+    public double getPrice() {
+        return PRICE;
+    }
+}
