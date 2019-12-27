d02ac518e47baecc41031a2550f51ef253b9f4da
Andriy <andriymattt@gmail.com>
2019-12-28 01:56:51 +0200

    feature/decorator: add the Client class to try how decorators work

---
+++ src/main/java/com/epam/Client.java
@@ -0,0 +1,44 @@
+package com.epam;
+
+import com.epam.bouquet.Bouquet;
+import com.epam.bouquet.enums.Flower;
+import com.epam.decorator.BouquetDecorator;
+import com.epam.decorator.impl.DeliveryDeco;
+import com.epam.decorator.impl.FlowerDeco;
+import com.epam.decorator.enums.DeliveryMethod;
+import com.epam.decorator.enums.DiscountCard;
+import com.epam.decorator.enums.PackagingMethod;
+import com.epam.decorator.impl.DiscountDeco;
+import com.epam.decorator.impl.PackagingDeco;
+import com.epam.bouquet.impl.StandardWeddingBouquet;
+
+import java.util.HashMap;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Bouquet standardBouquet = new StandardWeddingBouquet();
+        BouquetDecorator flowerDecorator = new FlowerDeco(Flower.SUNFLOWER, 3);
+        BouquetDecorator flowerDecorator2 = new FlowerDeco(Flower.IRIS, 5);
+        BouquetDecorator packaging = new PackagingDeco(PackagingMethod.PAPER_WRAPPER);
+        BouquetDecorator delivery = new DeliveryDeco(DeliveryMethod.ANOTHER_CITY);
+        BouquetDecorator discount = new DiscountDeco(DiscountCard.GOLD);
+
+        flowerDecorator.setBouquet(standardBouquet);
+        flowerDecorator2.setBouquet(flowerDecorator);
+        packaging.setBouquet(flowerDecorator2);
+        delivery.setBouquet(packaging);
+        discount.setBouquet(delivery);
+
+        BouquetDecorator fullBouquet = discount;
+
+        String name = fullBouquet.getName();
+        double cost = fullBouquet.getCost();
+        HashMap<Flower, Integer> flowers = fullBouquet.getFlowers();
+
+        System.out.println("name = " + name);
+        System.out.println("cost = " + cost);
+        System.out.println("flowers = " + flowers);
+    }
+
+}
