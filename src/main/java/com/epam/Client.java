d6f686580c56978163d4e912c811e039090cf38e
Andriy <andriymattt@gmail.com>
2019-12-29 01:39:29 +0200

    feature/factory: try order a bouquet in the Client class using factory classes

--- src/main/java/com/epam/Client.java
+++ src/main/java/com/epam/Client.java
@@ -1,6 +1,7 @@
 package com.epam;
 
 import com.epam.bouquet_catalogue.Bouquet;
+import com.epam.bouquet_catalogue.enums.BaseBouquetType;
 import com.epam.bouquet_catalogue.enums.Flower;
 import com.epam.bouquet_catalogue.impl.EmptyBouquet;
 import com.epam.decorator.BouquetDecorator;
@@ -11,6 +12,8 @@ import com.epam.decorator.enums.DiscountCard;
 import com.epam.decorator.enums.PackagingMethod;
 import com.epam.decorator.impl.DiscountDeco;
 import com.epam.decorator.impl.PackagingDeco;
+import com.epam.factory.Store;
+import com.epam.factory.impl.FlowerStore;
 
 import java.util.HashMap;
 
@@ -39,6 +42,27 @@ public class Client {
         System.out.println("name = " + name);
         System.out.println("cost = " + cost);
         System.out.println("flowers = " + flowers);
+        System.out.println("-----------------------------------");
+
+        Store store = new FlowerStore();
+
+        HashMap<Flower, Integer> additionalFlowers = new HashMap<>();
+
+        additionalFlowers.put(Flower.IRIS, 200);
+//        additionalFlowers.put(Flower.BLUEBELL, 7);
+//        additionalFlowers.put(Flower.SUNFLOWER, 11);
+        additionalFlowers.put(Flower.ROSE, 2);
+        additionalFlowers.put(Flower.TULIP,10);
+
+        Bouquet bouquet = store.order(BaseBouquetType.BIRTHDAY, additionalFlowers,
+                PackagingMethod.WITHOUT_PACKAGING, DeliveryMethod.WITHIN_CITY, DiscountCard.SOCIAL);
+
+        System.out.println("name= " + bouquet.getName());
+        System.out.println("cost= " + bouquet.getCost());
+        System.out.println("flowers= " + bouquet.getFlowers());
+        System.out.println(bouquet);
+
+
     }
 
 }
