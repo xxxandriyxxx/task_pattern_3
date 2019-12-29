36df347efb05a3ae184fb27b49ccd3a7fe763313
Andriy <andriymattt@gmail.com>
2019-12-29 18:26:45 +0200

    feature/mvc: add the Controller interface

---
+++ src/main/java/com/epam/controller/Controller.java
@@ -0,0 +1,35 @@
+package com.epam.controller;
+
+import com.epam.model.bouquet_catalogue.Bouquet;
+import com.epam.model.enums.*;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface Controller {
+
+    List<String> getStores();
+
+    Bouquet orderBouquet(int storeNumber, BaseBouquetType baseBouquetType, HashMap<Flower, Integer> additionalFlowers,
+                         PackagingMethod packagingMethod, DeliveryMethod deliveryMethod, DiscountCard discountCard);
+
+    Bouquet getBouquet(int storeNumber, BaseBouquetType baseBouquetType);
+
+    List<Flower> getFlowers();
+
+    BaseBouquetType getDefaultBaseBouquetType();
+
+    List<BaseBouquetType> getBaseBouquetTypes();
+
+    PackagingMethod getDefaultPackagingMethod();
+
+    List<PackagingMethod> getPackagingMethods();
+
+    DeliveryMethod getDefaultDeliveryMethod();
+
+    List<DeliveryMethod> getDeliveryMethods();
+
+    DiscountCard getDefaultDiscountCard();
+
+    List<DiscountCard> getDiscountCards();
+}
