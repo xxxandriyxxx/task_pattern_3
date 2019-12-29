479cd7e9cc7fb9821830194533fcf5902c3f96f8
Andriy <andriymattt@gmail.com>
2019-12-29 17:55:25 +0200

    feature/mvc: rename the FlowerStore class to FlowerStore1

--- src/main/java/com/epam/model/factory/impl/FlowerStore1.java
+++ src/main/java/com/epam/model/factory/impl/FlowerStore1.java
@@ -1,14 +1,14 @@
 package com.epam.model.factory.impl;
 
 import com.epam.model.bouquet_catalogue.Bouquet;
+import com.epam.model.bouquet_catalogue.impl.store1.StandardBirthdayBouquet1;
 import com.epam.model.enums.BaseBouquetType;
 import com.epam.model.bouquet_catalogue.impl.EmptyBouquet;
-import com.epam.model.bouquet_catalogue.impl.StandardBirthdayBouquet;
-import com.epam.model.bouquet_catalogue.impl.StandardValentinesBouquet;
-import com.epam.model.bouquet_catalogue.impl.StandardWeddingBouquet;
+import com.epam.model.bouquet_catalogue.impl.store1.StandardValentinesBouquet1;
+import com.epam.model.bouquet_catalogue.impl.store1.StandardWeddingBouquet1;
 import com.epam.model.factory.Store;
 
-public class FlowerStore extends Store {
+public class FlowerStore1 extends Store {
 
     @Override
     protected Bouquet createBouquet(BaseBouquetType baseBouquetType) {
@@ -16,11 +16,11 @@ public class FlowerStore extends Store {
         if(baseBouquetType == BaseBouquetType.EMPTY){
             bouquet = new EmptyBouquet();
         } else if(baseBouquetType == BaseBouquetType.BIRTHDAY){
-            bouquet = new StandardBirthdayBouquet();
+            bouquet = new StandardBirthdayBouquet1();
         } else if(baseBouquetType == BaseBouquetType.VALENTINES){
-            bouquet = new StandardValentinesBouquet();
+            bouquet = new StandardValentinesBouquet1();
         } else if(baseBouquetType == BaseBouquetType.WEDDING){
-            bouquet = new StandardWeddingBouquet();
+            bouquet = new StandardWeddingBouquet1();
         }
         return bouquet;
     }
